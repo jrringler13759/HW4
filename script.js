7f6732ea9df926b5e2a8b6586bad3dcfc32f9850
Nikita <jrringler13759@gmail.com>
2020-03-31 19:57:19 -0400

    started created functions

--- script.js
+++ script.js
@@ -7,46 +7,72 @@ var questions = [
 
 var timer = document.getElementById("timer");
 var qSpace = document.getElementById("question");
-var choiceA = document.getElementById("A");
-var choiceB = document.getElementById("B");
-var choiceC = document.getElementById("C");
-var choiceD = document.getElementById("D");
+var choices = document.getElementById("choices");
+var choiceA = document.getElementById("a");
+var choiceB = document.getElementById("b");
+var choiceC = document.getElementById("c");
+var choiceD = document.getElementById("d");
 var startBtn = document.getElementById("start");
 var rightWrong = document.getElementById("rightWrong");
 
+var secondsLeft = 75;
+
 function timer() {
     //timer counts down from 75 
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        timer.textContent = secondsLeft;
     //when it = 0 , game over/highscore screen
+        if(secondsLeft === 0) {
+          clearInterval(timerInterval);
+          
+        }
+    
+      }, 120000);
+    
 } 
 
-function showQuestion() {
+
+function showQuestion(qnum) {
     //shows a question and waits for an answer to be clicked
-    //when it gets to the end of the array of questions it
-    //sends the player to the highscore page.
-    //they enter their initials and it saves to local storage
+    
+       question[qnum];
+          qSpace.textContent = questions[qnum].question;
+          choiceA.textContent = questions[qnum].answers.a;
+          choiceB.textContent = questions[qnum].answers.b;
+          choiceC.textContent = questions[qnum].answers.c;
+          choiceD.textContent = questions[qnum].answers.d;
+          
 }
 
 function checkAnswer() {
     //when an answer is clicked it checks the answer
+    choices.addEventListener("click", function(event) {
+        var element = event.target;
+      
+        // If that element is a button...
+        if (element.matches("choiceA") && questions.correctAnswer === "a") {
+
+        } if (element.matches("choiceB") && questions.correctAnswer === "b") {
+            
+        } if (element.matches("choiceC") && questions.correctAnswer === "c") {
+            
+        } if (element.matches("choiceD") && questions.correctAnswer === "d") {
+            
+        }
     //compare to the property value from the question object
     //print right or wrong below the questsion
     //If correct gain points, if incorrect lose points
+});
 }
 
 
+
+var i = 0;
 function start() {
-    //hides the start button
-    //shows the question with answer choices for an interval
-    //and checks answer
-    //shows right or wrong and also shows new question on same click
-    
+    showQuestion(i);
+    i++
 }
 
 
-
-
-
-
-
-
-
+startBtn.addEventListener("click", start);
