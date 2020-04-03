b26770c72fa012a9d71688fffa58c49322d2d80d
Nikita <jrringler13759@gmail.com>
2020-04-03 15:48:21 -0400

    started high scores page js

--- script.js
+++ script.js
@@ -20,12 +20,16 @@ var scoreBoard = document.getElementById("score");
 
 var i = 0;
 var secondsLeft = 75;
-var score = 0;
 
-qSpace.style.display = "none";
-choices.style.display = "none";
+
+
+
+
+qSpace.style.display = "";
+choices.style.display = "";
 startBtn.style.display = "inline-grid";
 instructions.style.display = "inline-grid";
+
 
 
 function timer() {
@@ -40,10 +44,16 @@ function timer() {
   }, 1000);
 }
 
-//nothing is making this go to the next question. Do I need  for loop?
+//scoreBoard.textContent = " " + score;
+
+
+
+
+
+
 function showQuestion(qnum) {
     //shows a question 
-    choices.setAttribute("data-ans", questions[qnum].correctAnswer)
+    choices.dataset("data-ans", questions[qnum].correctAnswer)
     question[qnum];
         qSpace.textContent = questions[qnum].question;
         choiceA.textContent =  "A: " + questions[qnum].answers.a;
@@ -52,34 +62,33 @@ function showQuestion(qnum) {
         choiceD.textContent =  "D: " + questions[qnum].answers.d;
     
         if (qnum === 4){
-            console.log(qnum);
+        
         window.location.href = "highscores.html";
     }    
 }
 
+
+
+
 function resetQ () {
-    score++;
-    scoreBoard.textContent = " " + score;
+    rightWrong.textContent = "Correct!";
     i++;
+    if (i < 3){
     showQuestion(i);
-    
+  }  
 }
     //when an answer is clicked it checks the answer
-   choices.addEventListener("click", function(event) {
-        var element = event.target;
-        var correct = choices.getAttribute("data-ans");
-        console.log(correct)
+choices.addEventListener("click", function(event) {
+    var element = event.target;
+    var correct = choices.getAttribute("data-ans");
+    console.log(correct)
         if (element.getAttribute("id") === "a" && correct === "a") {
-            rightWrong.textContent = "Correct!";
             resetQ();
         } else if (element.getAttribute("id") === "b" && correct === "b") {
-            rightWrong.textContent = "Correct!"; 
             resetQ();
         } else if (element.getAttribute("id") === "c" && correct === "c") {
-            rightWrong.textContent = "Correct!";
             resetQ();
         } else if (element.getAttribute("id") === "d" && correct === "d") {
-            rightWrong.textContent = "Correct!";
             resetQ();
         } else{
             rightWrong.textContent = "Incorrect";
@@ -90,12 +99,6 @@ function resetQ () {
 });
 
 
-//I CAN'T GET THE NEXT QUESTION TO COME UP AFTER ANSWERING
-
-
-
-
-
 function hideShow() {
     qSpace.style.display = "inline-grid";
     startBtn.style.display = "none";
@@ -104,73 +107,90 @@ function hideShow() {
     quizTitle.style.display = "none";
 }
 
-
-
 function start() {
     hideShow();
     timer();
-    showQuestion(i);
-
-    
-    
+    showQuestion(i);   
+  
 }
-
 
 startBtn.addEventListener("click", start);
 
-// JavaScript for Highscored Page
-/*
+
+
+
+
+
+
+// JavaScript for High Scores Page
+
 var submitIn = document.getElementById("submitInitials");
 var highscoresInput = document.getElementById("highscoresInput");
 var goBack = document.getElementById("goBcack");
 var clearHs = document.getElementById("clearHs");
 var initials = document.getElementById("initials");
+var listOfHs = document.getElementById("listOfHs");
+
+var highScores = [];
+
+getHs();
 
 
 
-
-function renderHs() {
-    // Clear todoList element and update todoCountSpan
-    listOfHs.innerHTML = "";
-  
-    // Render a new li for each todo
-    for (var i = 0; i < todos.length; i++) {
-      var todo = todos[i];
-  
-      var hs = document.createElement("p");
-      li.textContent = todo;
-      todoList.appendChild(li);
-    }
+function listHs () {
+  for (var i = 0; i < highScores.length; i++){
+    var hs = highScores[i];
+    var li = document.createElement("li");
+    li.textContent = hs;
+    li.setAttribute("data-index", i);
+    listOfHs.appendChild(li);
   }
+}
 
-
-  function storeTodos() {
-    // Stringify and set "todos" key in localStorage to todos array
-    localStorage.setItem("todos", JSON.stringify(todos));
+function getHs () {
+  var storedHs = JSON.parse(localStorage.getItem("highScores"));
+  if (storedHs !== null){
+    highScores = storedHs;
   }
-  
-  // When form is submitted...
-  todoForm.addEventListener("submit", function(event) {
-    event.preventDefault();
-  
-    var todoText = todoInput.value.trim();
-  
-    // Return from function early if submitted todoText is blank
-    if (todoText === "") {
-      return;
-    }
-  
-    // Add new todoText to todos array, clear the input
-    todos.push(todoText);
-    todoInput.value = "";
-  
-    // Store updated todos in localStorage, re-render the list
-    storeTodos();
-    renderTodos();
-  });
-  
+  listHs();
+}
 
-//when they click submit the input and button dissapear and then the list appears with the appended entry
+function storeHs () {
+  localStorage.setItem("highScores", JSON.stringify(highScores));
+}
 
-//for loop? takes the entry and appends it with a 1 then a 2. and so on??
-*/
+submitIn.addEventListener("click", function(event){
+
+  var initialsInput = highScoresInput.value.trim();
+
+  if (initialsInput === "") {
+    return;
+  }
+  highScores.push(initialsInput);
+  highScoresInput.value = "";
+
+  storeHs();
+  listHs();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
